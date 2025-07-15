f376d96ebcfd7990ed07171696b862bb9290e6af
Adika Wicaksana <adikawicaksana@gmail.com>
2025-07-15 09:01:22 +0700

    Inital commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/portal/';
+    public string $baseURL = 'http://localhost/tna/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
